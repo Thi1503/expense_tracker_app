fb5fba5db5f02b35179f591b5729b5f9bd5d828d
Thi1503 <thile15032003@gmail.com>
2025-03-10 16:38:49 +0700

    add Responsiveness

---
+++ lib/widgets/expenses.dart
@@ -0,0 +1,108 @@
+import 'package:expense_tracker/widgets/chart/chart.dart';
+import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
+import 'package:expense_tracker/models/expense.dart';
+import 'package:expense_tracker/widgets/new_expense.dart';
+import 'package:flutter/material.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+
+  @override
+  State<Expenses> createState() {
+    return _ExpensesState();
+  }
+}
+
+class _ExpensesState extends State<Expenses> {
+  // tạo data ban đầu
+  final List<Expense> _registeredExpenses = [
+    Expense(
+        title: 'Flutter Course',
+        amount: 15.99,
+        date: DateTime.now(),
+        category: Category.work),
+    Expense(
+        title: 'Cinema',
+        amount: 19.99,
+        date: DateTime.now(),
+        category: Category.leisure),
+  ];
+
+  // Hiển thị phần thêm mục chi phí mới
+  void _openExpenseOverLay() {
+    showModalBottomSheet(
+        isScrollControlled: true,
+        context: context,
+        builder: (ctx) => NewExpense(onAddExpense: _addExpense));
+  }
+
+  // Thêm chi phí mới
+  void _addExpense(Expense expense) {
+    setState(() {
+      _registeredExpenses.add(expense);
+    });
+  }
+
+  // Xóa chi phí
+  void _removeExpense(Expense expense) {
+    final expenseIndex = _registeredExpenses.indexOf(expense);
+    setState(() {
+      _registeredExpenses.remove(expense);
+    });
+
+    ScaffoldMessenger.of(context).clearSnackBars();
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        duration: const Duration(seconds: 3),
+        content: const Text('Exoense deleted.'),
+        action: SnackBarAction(
+          label: 'Undo',
+          onPressed: () {
+            setState(() {
+              _registeredExpenses.insert(expenseIndex, expense);
+            });
+          },
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(context) {
+    final widthScreen = MediaQuery.of(context).size.width;
+    final heightScreen = MediaQuery.of(context).size.height;
+
+    Widget mainContent =
+        const Center(child: Text('No expense found. Start add expenses'));
+
+    if (_registeredExpenses.isNotEmpty) {
+      mainContent = ExpensesList(
+          expenses: _registeredExpenses, onRemoveExpense: _removeExpense);
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter ExpenseTracker'),
+        actions: [
+          IconButton(
+            onPressed: _openExpenseOverLay,
+            icon: const Icon(Icons.add),
+          )
+        ],
+      ),
+      body: widthScreen > heightScreen
+          ? Row(children: [
+              Expanded(child: Chart(expenses: _registeredExpenses)),
+              // hiển thị danh sách chi phí
+              Expanded(child: mainContent),
+            ])
+          : Column(
+              children: [
+                Chart(expenses: _registeredExpenses),
+                // hiển thị danh sách chi phí
+                Expanded(child: mainContent),
+              ],
+            ),
+    );
+  }
+}
